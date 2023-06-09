9dea854cb37ee3c5d35cdf65f904eea8efe30026
BashirMahmood <bashirsaiwan85@gmail.com>
2023-06-08 19:40:10 -0500

    1st push q1-q4

---
+++ SQL Scripts/Q2.sql
@@ -0,0 +1,8 @@
+--How many rows are in the data_analyst_jobs table?
+
+-- SELECT COUNT (*)
+-- FROM data_analyst_jobs
+
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10;
---
+++ SQL Scripts/Q4.sql
@@ -0,0 +1,25 @@
+--Q1/ How many rows are in the data_analyst_jobs table?
+
+-- SELECT COUNT (*)
+-- FROM data_analyst_jobs
+
+--Q2/ Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
+
+-- SELECT *
+-- FROM data_analyst_jobs
+-- LIMIT 10;
+
+--Q3/ How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+
+-- SELECT COUNT (*)
+-- FROM data_analyst_jobs 
+-- WHERE location = 'TN' OR location = 'KY'
+
+
+--Q4/ How many postings in Tennessee have a star rating above 4?
+
+SELECT COUNT (*)
+FROM data_analyst_jobs 
+WHERE location = 'TN'
+AND star_rating > 4;
+
